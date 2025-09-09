396a4278a44a36b74d850ce262a279b91b55b23e
ratiorine1417 <rataventurine@gmail.com>
2025-09-09 12:04:35 +0900

    Delete src directory

--- src/api/projects.ts
+++
@@ -1,7 +0,0 @@
-﻿import { http } from "@/api/http";
-import type { ProjectListDto } from "@/types/domain";
-
-export async function listProjects() {
-  const { data } = await http.get<ProjectListDto[]>("/projects");
-  return data;
-}
